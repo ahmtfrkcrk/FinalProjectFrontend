05cd28cebfbccc0f3d15237d529b0c7d840f9ab4
FARUK <FARUK@FARUKPC>
2023-06-05 00:56:41 +0300

    commit

---
+++ src/app/services/product.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import { ProductResponseModel } from '../models/productResponseModel';
+import { Observable } from 'rxjs';
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductService {
+
+  apiUrl="https://localhost:44389/api/products/getall";
+  
+  constructor(private httpclient:HttpClient) { }
+
+  getProducts():Observable<ProductResponseModel>{
+    return this.httpclient.get<ProductResponseModel>(this.apiUrl);
+  }
+}
