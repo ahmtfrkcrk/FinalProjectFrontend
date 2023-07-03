3ccc59d6316bd3e10bf1b5068e25c3c05e7b3de4
FARUK <FARUK@FARUKPC>
2023-07-04 01:27:22 +0300

    3 haziran commit 1

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -3,6 +3,7 @@ import {HttpClient} from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { ListResponseModel } from '../models/listResponseModel';
 import { Product } from '../models/product';
+import { ResponseModel } from '../models/responseModel';
 @Injectable({
   providedIn: 'root'
 })
@@ -10,14 +11,17 @@ export class ProductService {
 
   apiUrl="https://localhost:44389/api/";
   
-  constructor(private httpclient:HttpClient) { }
+  constructor(private httpClient:HttpClient) { }
 
   getProducts():Observable<ListResponseModel<Product>>{
     let newPath = this.apiUrl + "products/getall";
-    return this.httpclient.get<ListResponseModel<Product>>(newPath);
+    return this.httpClient.get<ListResponseModel<Product>>(newPath);
   }
   getProductsByCategory(categoryId:number):Observable<ListResponseModel<Product>>{
     let newPath = this.apiUrl + "products/getbycategory?categoryid="+categoryId;
-    return this.httpclient.get<ListResponseModel<Product>>(newPath);
+    return this.httpClient.get<ListResponseModel<Product>>(newPath);
+  }
+  add(product:Product):Observable<ResponseModel>{
+    return this.httpClient.post<ResponseModel>(this.apiUrl+"products/add",product);
   }
 }
